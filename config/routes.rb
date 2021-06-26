a0d2db742c061f8e25ea6935f0e256c4dd1a4792
Benjamin ROUDIERE <benjamin.roudiere@agricourtage.fr>
2021-06-26 18:09:41 +0200

    watch list

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # A user can see all the lists
+  # GET "lists"
+  # A user can see the details of a given list and its name
+  # GET "lists/42"
+  # A user can create a new list
+  # GET "lists/new"
+  # POST "lists"
+
+  # A user can add a new bookmark (movie/list pair) to an existing list
+
+  root to: 'lists#index'
+  resources :lists, except: %i[edit update] do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  resources :bookmarks, only: :destroy
 end
